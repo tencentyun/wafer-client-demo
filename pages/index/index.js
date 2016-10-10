ae736c3a0ad3402c407f1b8d3228b64ebbf63aa7
techird <techird@qq.com>
2016-10-10 19:41:50 +0800

    更新 Demo

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,58 +2,44 @@ var qcloud = require('../../node_modules/qcloud-weapp-client-sdk/index.js');
 
 Page({
     data: {
-        loginUrl: wx.getStorageSync('loginUrl') || 'https://www.qcloud.la/applet/sdk/login',
-        requestUrl: wx.getStorageSync('requestUrl') || 'https://www.qcloud.la/applet/sdk/request'
-    },
-
-    changeData: function(name, value) {
-        this.setData({
-            [name]: value
-        });
-        wx.setStorageSync(name, value);
-    },
-
-    changeLoginUrl: function(e) {
-        this.changeData('loginUrl', e.detail.value);
-    },
-
-    changeRequestUrl: function(e) {
-        this.changeData('requestUrl', e.detail.value);
+        loginUrl: 'https://www.qcloud.la/login',
+        requestUrl: 'https://www.qcloud.la/user'
     },
 
     doLogin: function () {
-        setTimeout(() => {
-            qcloud.login({
-                loginUrl: this.data.loginUrl,
+        qcloud.setLoginUrl(this.data.loginUrl);
+        qcloud.login({
 
-                success: function () {
-                    console.log('login success', arguments);
-                },
+            success: function () {
+                console.log('login success', arguments);
+            },
 
-                fail: function () {
-                    console.log('login fail', arguments);
-                }
-            });
-        }, 1)
+            fail: function () {
+                console.log('login fail', arguments);
+            }
+        });
+    },
+
+    clearSession: function() {
+        wx.clearStorageSync();
     },
 
     doRequest: function () {
-        setTimeout(() => {
-            qcloud.request({
-                url: this.data.requestUrl,
+        qcloud.setLoginUrl(this.data.loginUrl);
+        qcloud.request({
+            url: this.data.requestUrl,
 
-                success: function () {
-                    console.log('request success', arguments);
-                },
+            success: function () {
+                console.log('request success', arguments);
+            },
 
-                fail: function () {
-                    console.log('request fail', arguments);
-                },
+            fail: function () {
+                console.log('request fail', arguments);
+            },
 
-                complete: function () {
-                    console.log('request complete');
-                }
-            });
+            complete: function () {
+                console.log('request complete');
+            }
         });
     },
 });

284d651f72eeea55f4088aeff932737b6ef5606f
techird <techird@qq.com>
2016-10-10 15:43:40 +0800

    测试地址支持编辑和保存

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,35 +1,59 @@
 var qcloud = require('../../node_modules/qcloud-weapp-client-sdk/index.js');
 
 Page({
-    doLogin: function () {
-        qcloud.login({
-            loginUrl: 'https://www.qcloud.la/applet/sdk/login',
+    data: {
+        loginUrl: wx.getStorageSync('loginUrl') || 'https://www.qcloud.la/applet/sdk/login',
+        requestUrl: wx.getStorageSync('requestUrl') || 'https://www.qcloud.la/applet/sdk/request'
+    },
 
-            success: function () {
-                console.log('success', arguments);
-            },
-
-            fail: function () {
-                console.log('fail', arguments);
-            }
+    changeData: function(name, value) {
+        this.setData({
+            [name]: value
         });
+        wx.setStorageSync(name, value);
+    },
+
+    changeLoginUrl: function(e) {
+        this.changeData('loginUrl', e.detail.value);
+    },
+
+    changeRequestUrl: function(e) {
+        this.changeData('requestUrl', e.detail.value);
+    },
+
+    doLogin: function () {
+        setTimeout(() => {
+            qcloud.login({
+                loginUrl: this.data.loginUrl,
+
+                success: function () {
+                    console.log('login success', arguments);
+                },
+
+                fail: function () {
+                    console.log('login fail', arguments);
+                }
+            });
+        }, 1)
     },
 
     doRequest: function () {
-        qcloud.request({
-            url: 'https://www.qcloud.la/applet/sdk/request',
+        setTimeout(() => {
+            qcloud.request({
+                url: this.data.requestUrl,
 
-            success: function () {
-                console.log('success', arguments);
-            },
+                success: function () {
+                    console.log('request success', arguments);
+                },
 
-            fail: function () {
-                console.log('fail', arguments);
-            },
+                fail: function () {
+                    console.log('request fail', arguments);
+                },
 
-            complete: function () {
-                console.log('complete', arguments);
-            }
+                complete: function () {
+                    console.log('request complete');
+                }
+            });
         });
     },
 });
